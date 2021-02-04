8eb401728121286f9565bca32440eaed49651328
PatrikJMelander <patrikjmelander@gmail.com>
2021-02-04 09:19:14 +0100

    Category and ProductCategory ready

---
+++ src/main/java/shoeWebshop/model/Category.java
@@ -0,0 +1,13 @@
+package shoeWebshop.model;
+
+/**
+ * Created by Patrik Melander
+ * Date: 2021-02-04
+ * Time: 09:16
+ * Project: Best_shoe_webshop_ever
+ * Copyright: MIT
+ */
+public class Category {
+    int id;
+    String CategoryName;
+}
---
+++ src/main/java/shoeWebshop/model/ProductCategory.java
@@ -0,0 +1,14 @@
+package shoeWebshop.model;
+
+/**
+ * Created by Patrik Melander
+ * Date: 2021-02-04
+ * Time: 09:17
+ * Project: Best_shoe_webshop_ever
+ * Copyright: MIT
+ */
+public class ProductCategory {
+    int id;
+    Product productId;
+    Category categoryId;
+}
